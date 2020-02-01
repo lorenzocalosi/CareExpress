2fcc288e446c0c37333fd5465ce255aca424a571
Domiziano <domizianoatticciati@gmail.com>
2020-02-01 03:08:02 +0100

    Changed Movement, Rewired pref, Added EventHotSpot
    Modified movement function, added interact button in rewired prefab and modified RewiredConsts accordingly, added a prototype of eventhotSpot

---
+++ Assets/Scripts/EventHotSpot.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class EventHotSpot : MonoBehaviour
+{
+	#region Fields
+
+	// Static
+
+	// Public
+	public Event.Event theEvent;
+	// Hidden Public
+	private PlayerMovement playerMovement;
+	// Properties
+
+	// Components
+
+	// Events
+
+	#endregion
+
+	#region Unity Callbacks
+
+	private void OnEnable()
+	{
+		playerMovement = FindObjectOfType<PlayerMovement>();
+		playerMovement.OnEnteredTriggerEvent += CallShowEvent;
+	}
+
+	private void OnDisable()
+	{
+		playerMovement.OnEnteredTriggerEvent -= CallShowEvent;
+	}
+
+	#endregion
+
+	#region Methods
+
+	private void CallShowEvent()
+	{
+		//Debug.Log($"Event Recived");
+		Event.EventHandler.Instance.ShowEvent(theEvent);
+	}
+
+	#endregion
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,20 +11,37 @@ public class PlayerMovement : MonoBehaviour
 	private int playerID = 0;
 
 	private float x, y;
+	private bool isInteracting;
 	private Rigidbody2D rb;
 
-	// Start is called before the first frame update
+	public event Action OnEnteredTriggerEvent;
+
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
 		player = ReInput.players.GetPlayer(playerID);
 		player.AddInputEventDelegate(GetX, UpdateLoopType.Update, InputActionEventType.AxisActiveOrJustInactive, RewiredConsts.Action.Horizontal_Movement);
 		player.AddInputEventDelegate(GetY, UpdateLoopType.Update, InputActionEventType.AxisActiveOrJustInactive, RewiredConsts.Action.Vertical_Movement);
+		player.AddInputEventDelegate(GetInteract, UpdateLoopType.Update, InputActionEventType.ButtonPressed, RewiredConsts.Action.Interact);
 	}
-
 	private void FixedUpdate()
 	{
+		if (isInteracting)
+		{
+			Interact();
+		}
+		
 		Move();
+		SetRotation();
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.GetComponent<EventHotSpot>())
+		{
+			//Debug.Log($"event invoked");
+			OnEnteredTriggerEvent?.Invoke();
+		}
 	}
 
 	private void GetX(InputActionEventData i)
@@ -36,17 +53,32 @@ public class PlayerMovement : MonoBehaviour
 	{
 		y = i.GetAxis();
 	}
-	
+
+	private void GetInteract(InputActionEventData i)
+	{
+		isInteracting = i.GetButtonDown(); 
+	}
+
+	private void Interact()
+	{
+		Debug.Log($"{isInteracting}");
+	}
 	private void Move()
 	{
-		if(x == 0 && y == 0) return;
-		Vector2 prevPos = transform.position;
-		rb.MovePosition(new Vector2(x,y) + prevPos);
+		//if(x == 0 && y == 0) return;
+		Vector2 prevPos = rb.transform.position;
+		rb.velocity = ((new Vector2(x, y)) * speed);
 	}
 
 	private void SetRotation()
 	{
-		Vector2 direction = rb.velocity - (Vector2)transform.position;
-		transform.LookAt(direction);
+		Vector2 direction = (new Vector2(x, y)).normalized;
+
+		if (direction != Vector2.zero)
+		{
+			float correctionAngleBasedOnSprite = -90; // valore da twiccare in base all'angolazione della sprite
+			float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle + correctionAngleBasedOnSprite, Vector3.forward);
+		}
 	}
 }
