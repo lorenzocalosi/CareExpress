0d5291f66115989e1011ff8d329b85de7d5660af
Calosi <lorenzo.calosi1@gmail.com>
2020-02-02 14:13:38 +0100

    Added Ending Screen and Renamed Actors

---
+++ Assets/EndingScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Event;
+using Sirenix.OdinInspector;
+
+public class EndingScreen : SerializedMonoBehaviour
+{
+	public Dictionary<Actor, List<Image>> images;
+
+    public void ShowActorImages(List<Actor> actors) {
+		for (int i = 0; i < actors.Count; i++) {
+			if (images.ContainsKey(actors[i])) {
+				foreach (Image image in images[actors[i]]) {
+					image.color = Color.white;
+				}
+			}
+		}
+	}
+}
